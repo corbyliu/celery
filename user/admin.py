dfeb65b6cd149ac7e36675a532c93d517cb85d4b
liuss <liuss@udsafe.com.cn>
2020-03-05 11:00:09 +0800

    添加gitignore

--- user/admin.py
+++ user/admin.py
@@ -12,4 +12,60 @@ from user.models import User
 class UserCreationForm(forms.ModelForm):
 
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    pass
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm.self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'phone', 'is_active', 'is_admin')
+
+    def clean_password(self):
+        return self.initial["password"]
+
+
+class MyUserAdmin(UserAdmin):
+
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'phone', 'is_admin')
+    list_filter = ('is_admin',)
+    fields = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal_info', {'fields': ('phone',)}),
+        ('Permissions', {'fields': ('is_admin',)})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'phone', 'password1', 'password2')
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
